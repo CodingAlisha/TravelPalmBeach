1022a99c0c586516dbe64f65e0ad2843abe05cba
CodingAlisha <alishasanguinetti@gmail.com>
2026-05-16 16:09:03 -0400

    device view updated

---
+++ src/components/Hamburger/Hamburger.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+import './Hamburger.css';
+
+const Hamburger = () => {
+  return (
+    <div className='hamburger'>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+    </div>
+  )
+}
+
+export default Hamburger;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,31 +4,98 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css';
 import Logo from '../Logo/Logo';
-import Home from '../../pages/Home/Home';
-import Dining from '../../pages/Dining/Dining';
+import Hamburger from '../Hamburger/Hamburger';
+import { useState } from 'react';
+import closeHamburger from '..//..//assets/closeHamburger.png';
 
 
 const Navbar = () => {
 
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    // console.log('clicked');
+    setHamburgerOpen(!hamburgerOpen)
+  }
+
+ 
+
 
   return (
     <div className='navbarContainer'>
-      
-      
-     
       <nav className='navbar'>
+
+      {/* <div style={{color: 'red'}}>{hamburgerOpen ? 'OPEN' : 'CLOSED'}</div> */}
+      
+      <div className='hamburger' onClick={toggleHamburger}>
+          {/* <Hamburger isOpen={hamburgerOpen}/> */}
+          {hamburgerOpen ? (
+            
+              <img 
+              src={closeHamburger}
+              alt='close Menu'
+              className='closeHamburger'
+              />) : <Hamburger />}
+            
+        </div>
+
         <Logo />
+
+        <ul className={hamburgerOpen ? 'navLinks active' : 'navLinks'}>
+         
+         
+          <li><Link to='/'>HOME</Link></li>
+          <li><Link to='/Dining'>DINING</Link></li>
+          <li><Link to='/Romance'>ROMANCE</Link></li>
+          <li><Link to='/Adventure'>ADVENTURE</Link></li>
+          <li><Link to='/Hotel'>HOTEL</Link></li>
+          <li><Link to='/Explore'>EXPLORE</Link></li>
+         
+        </ul>
     
-        <Link to='/'>HOME</Link>
-        <Link to='/Dining'>DINING</Link>
-        {/* <Link to='/Landing'>LANDING</Link> */}
-        <Link to='/Romance'>ROMANCE</Link>
-        <Link to='/Adventure'>ADVENTURE</Link>
-        <Link to='/Hotel'>HOTEL</Link>
-        <Link to='/Explore'>EXPLORE ALL</Link>
       </nav>
+
+      <div>
+      
+      </div>
     </div>
   )
 }
 
 export default Navbar;
+
+
+
+
+
+// return (
+//   <div className='navbarContainer'>
+    
+    
+   
+//     <nav className='navbar'>
+
+//       <ul className={hamburgerOpen ? 'navLinks active' : 'navLinks'}>
+//         <Logo />
+
+//         <Hamburger />
+       
+//         <li><Link to='/'>HOME</Link></li>
+//         <li><Link to='/Dining'>DINING</Link></li>
+//         <li><Link to='/Romance'>ROMANCE</Link></li>
+//         <li><Link to='/Adventure'>ADVENTURE</Link></li>
+//         <li><Link to='/Hotel'>HOTEL</Link></li>
+//         <li><Link to='/Explore'>EXPLORE</Link></li>
+       
+//       </ul>
+  
+//     </nav>
+
+//     <div>
+//       <div className='hamburger' onClick={toggleHamburger}>
+//         <Hamburger  on={hamburgerOpen}/>
+//       </div>
+//     </div>
+//   </div>
+// )
+// }
